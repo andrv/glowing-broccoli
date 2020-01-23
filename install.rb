6f09d094d206100d4698aab83c7ea072acb72e95
Андрей <andrej.vinokurov@gmail.com>
2020-01-23 22:56:27 +0100

    Begin implementation

--- install.rb
+++ install.rb
@@ -1,1 +1,20 @@
 #!/usr/bin/env ruby
+
+home = Dir.home
+vim_dir = "#{home}/.vim"
+vimrc   = "#{home}/.vimrc"
+vim_dir_bkp = "#{home}/vim.bkp"
+vimrc_bkp   = "#{home}/vimrc.bkp"
+
+should_backup = File.exist?(vim_dir) || File.exist?(vimrc_bkp)
+backup_exists = File.exist?(vim_dir_bkp) || File.exist?(vimrc_bkp)
+
+text_msg = "Will create new '#{vim_dir}' and '#{vimrc}'.\n"
+
+if should_backup
+  text_msg += "Existing '#{vim_dir}' and '#{vimrc}' will be backuped to '#{vim_dir_bkp}' and '#{vimrc_bkp}' respectively."
+else
+  # no backup
+end
+
+puts text_msg
