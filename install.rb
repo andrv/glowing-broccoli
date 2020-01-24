21fbc67ad4508b1e6450c3594fc0b7e42b463dad
Андрей <andrej.vinokurov@gmail.com>
2020-01-24 18:12:25 +0100

    Add right aoutput

--- install.rb
+++ install.rb
@@ -9,12 +9,21 @@ vimrc_bkp   = "#{home}/vimrc.bkp"
 should_backup = File.exist?(vim_dir) || File.exist?(vimrc_bkp)
 backup_exists = File.exist?(vim_dir_bkp) || File.exist?(vimrc_bkp)
 
-text_msg = "Will create new '#{vim_dir}' and '#{vimrc}'.\n"
+text_blocks = {
+  message:  "Will create new '#{vim_dir}' and '#{vimrc}'.\n",
+  backup:   "Restore possible from existing backup.\n",
+  question: "What is to do? (r - restore, i - install):\n",
+}
 
 if should_backup
-  text_msg += "Existing '#{vim_dir}' and '#{vimrc}' will be backuped to '#{vim_dir_bkp}' and '#{vimrc_bkp}' respectively."
-else
-  # no backup
+  text_blocks[:message] += "Existing vim files will be backuped.\n"
 end
 
-puts text_msg
+text_to_print = text_blocks[:message]
+
+if backup_exists
+  text_to_print += text_blocks[:backup]
+  text_to_print += text_blocks[:question]
+end
+
+puts text_to_print
